93cdfa14496dd1da66153180993ce653a8c11761
Julien Yensen Martin <julien-yensen.martin@quebecormedia.com>
2020-01-30 14:14:36 -0500

    Fix

--- charms/wordpress/reactive/wordpress.py
+++ charms/wordpress/reactive/wordpress.py
@@ -1,7 +1,14 @@
-from charms.reactive import when, when_not, set_flag
+from charms.reactive import when, when_not, set_flag, hook
+from charmhelpers.core.hookenv import status_set
+import charms.apt
 
+@when_not('apt.installed.wordpress')
+def install_wordpress_apt():
+    charms.apt.queue_install(['wordpress'])
+    #sets the 'apt.installed.wordpress' flag when done
 
-@when_not('wordpress.installed')
+@when('apt.installed.wordpress')
+@when_not('wordpress.ready')
 def install_wordpress():
     # Do your setup here.
     #
@@ -14,4 +21,15 @@ def install_wordpress():
     #  * https://jujucharms.com/docs/devel/developer-getting-started
     #  * https://github.com/juju-solutions/layer-basic#overview
     #
-    set_flag('wordpress.installed')
+    status_set('blocked', "wordpress installed, waiting for database")
+    set_flag('wordpress.ready')
+
+@when_not('wordpress.ready')
+@when_not('apt.installed.wordpress')
+def waiting_for_wordpress():
+    status_set('maintenance', "waiting for apt wordpress installation")
+
+@hook('database-relation-joined')
+def database_is_ready():
+    status_set('blocked', 'Database is ready (joined) but not configured')
+    set_flag('wordpress.database_is_ready')
